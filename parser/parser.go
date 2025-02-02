0e2f3130578e9f5d24ff6b9d33af71862c5de343
Stefan Alfbo <stefan@alfbo.se>
2025-02-02 16:15:09 +0100

    Add a parser package and add an initial draft of a parser

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"github.com/stefanalfbo/monkey/ast"
+	"github.com/stefanalfbo/monkey/lexer"
+	"github.com/stefanalfbo/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so currentToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
