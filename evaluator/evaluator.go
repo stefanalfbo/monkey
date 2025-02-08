3f2e8dc02f7855eecc92f44b3fff7503681982c9
Stefan Alfbo <stefan@alfbo.se>
2025-02-08 10:32:00 +0100

    Begin to build the eval code for the monkey language

---
+++ evaluator/evaluator.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"github.com/stefanalfbo/monkey/ast"
+	"github.com/stefanalfbo/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
+}
