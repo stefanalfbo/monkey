3416ee7fc591d51d5d254b0fe57f645ce7fb83a3
Stefan Alfbo <stefan@alfbo.se>
2025-02-12 20:23:28 +0100

    Start implementing support for arrays, begin with updating the lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -32,6 +32,8 @@ if (5 < 10) {
 
 "foobar"
 "foo bar"
+
+[1, 2];
 `
 
 	tests := []struct {
@@ -113,6 +115,12 @@ if (5 < 10) {
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
