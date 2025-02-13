2ec91d0b05cc1d9cf8dd5697f53e38ec5c2cae1d
Stefan Alfbo <stefan@alfbo.se>
2025-02-13 21:26:22 +0100

    Add a builtin function for printing, 'puts'

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/stefanalfbo/monkey/object"
+import (
+	"fmt"
+
+	"github.com/stefanalfbo/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -96,6 +100,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
